21d6f39304b0286b739c71087376fe403b191d39
Florian Schweizer <post.an.flo@gmail.com>
2021-11-05 16:04:50 +0100

    Initial Commit

---
+++ Calcul/CalculApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculApp.swift
+//  Calcul
+//
+//  Created by florian schweizer on 05.11.21.
+//
+
+import SwiftUI
+
+@main
+struct CalculApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Calcul/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Calcul
+//
+//  Created by florian schweizer on 05.11.21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
